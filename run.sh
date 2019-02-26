cc03690ad16886efc94725f270ccce88c8826f4e
nanfei <yenanfei@momenta.ai>
2019-02-26 18:50:04 +0800

    使用fcitx解决中文输入的问题

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "You should have docker environment"
 if sudo apt-get install x11-xserver-utils -y;then
 	:
 else
@@ -8,4 +9,16 @@ fi
 xhost + &>/dev/null
 docker rm -f docker-wine
 docker pull xs020105/docker-wine
-docker run -ti -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -e GDK_SCALE -e GDK_DPI_SCALE --name docker-wine --privileged=true xs020105/docker-wine /bin/bash
+docker run -ti -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e XMODIFIERS=@im=fcitx \
+    -e QT_IM_MODULE=fcitx \
+    -e GTK_IM_MODULE=fcitx \
+    -e AUDIO_GID=`getent group audio | cut -d: -f3` \
+    -e VIDEO_GID=`getent group video | cut -d: -f3` \
+    -e GID=`id -g` \
+    -e UID=`id -u` \
+	-e DISPLAY=unix$DISPLAY \
+	-e GDK_SCALE \
+	-e GDK_DPI_SCALE \
+	--name docker-wine \
+	--privileged=true xs020105/docker-wine /bin/bash
