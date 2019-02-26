ed7edb2171a38462ed503efc06d0864dbd21cdff
nanfei <yenanfei@momenta.ai>
2019-02-26 16:37:26 +0800

    first commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if sudo apt-get install x11-xserver-utils -y;then
+	:
+else
+	echo "I can't install x11-xserver-utils,please manual install it!"
+	exit 1
+fi
+xhost + &>/dev/null
+docker run -ti -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY  -e GDK_SCALE -e GDK_DPI_SCALE --name docker-wine-$RANDOM xs020105/docker-wine /bin/bash
